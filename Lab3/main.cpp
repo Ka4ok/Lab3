73c9eec1b4a971ddadccba2890075ce60695d1a9
Ka4ok <mityan98@rambler.ru>
2019-10-10 20:14:55 +0300

    +qt

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,4 +1,10 @@
 #include "function.h"
+#include "qwidgets.h"
+#include "qcore.h"
+#include "qmath.h"
+#include "qgl.h"
+
+//#include "wxwidjets"
 int main(int argc, char** argv)
 {
 	hello_world();
